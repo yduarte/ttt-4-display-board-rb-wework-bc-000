d7e19100dc421b8ed1d140c58b0d174b8c36c1b4
Yanllerline Duarte <yanllerline.duarte@wework.com>
2018-03-04 18:00:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board=["O"," "," "," ","X"," "," "," "," "]
+board=["X","X","X"," ","X"," "," "," "," "]
 
 def display_board(board)
-  puts " #{board[0]} |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
