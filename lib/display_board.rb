1947517fd500a51a25d92ab64618cbe67d8f24c3
Yanllerline Duarte <yanllerline.duarte@wework.com>
2018-03-04 18:03:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_board(board)
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8] "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(board)
